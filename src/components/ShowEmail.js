522c22701989ca0eb6503e6b200127392569290a
devendraSaroj <devendrasaroj97@gmail.com>
2020-04-23 14:08:19 +0530

    final commit

---
+++ src/components/ShowEmail.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import iconArrow02 from '../media/icon_arrow02.svg';
+
+import './ShowEmail.css';
+
+export default function ShowEmail(props) {
+
+  return (
+      <div className="email">
+        <div
+          className="email__go-back"
+          onClick={ ()=> props.setViewEmailID(null) }>
+          <img
+            className="icon-back"
+            src={ iconArrow02 } alt="Icon back" />
+            Go back
+        </div>
+        <div>
+          <div className="email__field"><strong>From</strong><br/><br/>{ props.email.from }</div>
+          <div className="email__field"><strong>To</strong><br/><br/>{ props.email.to.join(", ") }</div>
+          <div className="email__field"><strong>Subject</strong><br/><br/>{ props.email.subject }</div>
+          { props.email.attachment.length > 0 && <div className="email__field"><strong>Attachments</strong><br/><br/>{ props.email.attachment.join(", ") }</div>}
+          <div className="email__field"><strong>Body</strong><br/><br/>{ props.email.body }</div>
+        </div>
+      </div>
+  )
+}
