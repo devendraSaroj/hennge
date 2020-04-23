fb706502db012eae7c3db8d6ff0a0ec84edae48f
devendraSaroj <devendrasaroj97@gmail.com>
2020-04-23 16:21:47 +0530

    Updates

---
+++ precache-manifest.17b8ca9ea8d1cae4c0f0b15148443efd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2cc9074c449feed2eb78bd9837134e",
+    "url": "/hennge/index.html"
+  },
+  {
+    "revision": "9e99f5ccc9b43bfa8efa",
+    "url": "/hennge/static/css/2.7c7f2721.chunk.css"
+  },
+  {
+    "revision": "b551073f7dbd3ef12ccc",
+    "url": "/hennge/static/css/main.c8d9ac4d.chunk.css"
+  },
+  {
+    "revision": "9e99f5ccc9b43bfa8efa",
+    "url": "/hennge/static/js/2.07dddd27.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/hennge/static/js/2.07dddd27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b551073f7dbd3ef12ccc",
+    "url": "/hennge/static/js/main.39e68d30.chunk.js"
+  },
+  {
+    "revision": "bd0d9124d467fd99431c",
+    "url": "/hennge/static/js/runtime-main.6f860ab6.js"
+  },
+  {
+    "revision": "c7f1a998dbd248dce8371c40700c32c6",
+    "url": "/hennge/static/media/icon_arrow01.c7f1a998.svg"
+  },
+  {
+    "revision": "a8497fb7c109e4c9768cbd8de8cab045",
+    "url": "/hennge/static/media/icon_arrow02.a8497fb7.svg"
+  },
+  {
+    "revision": "e058455f8e2f925d5a381b6d564c8487",
+    "url": "/hennge/static/media/icon_calender.e058455f.svg"
+  },
+  {
+    "revision": "0c024d95d4e0abaee69600cfdaebc5be",
+    "url": "/hennge/static/media/icon_clip.0c024d95.svg"
+  },
+  {
+    "revision": "b845eca9ae900345cc75533dca07b2db",
+    "url": "/hennge/static/media/icon_mail_sp.b845eca9.svg"
+  },
+  {
+    "revision": "04846dcdb42b086080b88f4a4fefa4d1",
+    "url": "/hennge/static/media/icon_search.04846dcd.svg"
+  }
+]);
